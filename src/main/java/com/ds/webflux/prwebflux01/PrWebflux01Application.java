ecb595667c9141e668fcd6d5a90e1c4598030f2d
Didney <didneynie12@gmail.com>
2026-01-19 17:56:11 -0500

    initial commit

---
+++ src/main/java/com/ds/webflux/prwebflux01/PrWebflux01Application.java
@@ -0,0 +1,13 @@
+package com.ds.webflux.prwebflux01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrWebflux01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrWebflux01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ds/webflux/prwebflux01/PrWebflux01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.webflux.prwebflux01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrWebflux01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
